b2cbc34cde08147686a5dd859679b669cd263ff7
MichelleCoffey <michellembcoffey@icloud.com>
2023-05-05 07:31:30 +0000

    added weather api and landing page layout.

--- cfg-weather-app/src/App.js
+++ cfg-weather-app/src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import './App.css';
+const api = {
+  key: "36ffa85294264ac7d94239e6938bd968",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+          />
+        </div>
+        <div className="location-box">
+          <div className="location"></div>
+          <div className="date"></div>
+        </div>
+        <div className="weather-box">
+          <div className="temp"></div>
+          <div className="weather"></div>
+        </div>
+      </main>
     </div>
   );
 }
